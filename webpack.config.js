6afded67df2b71d05e1bec45bf867b7e063bc140
Sridhar <sridhar6668@gmail.com>
2022-03-31 22:07:32 -0700

    install typescript

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,11 @@ module.exports = {
     module: {
         rules: [
             {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: 'ts-loader',
+            },
+            {
                 test: /\.(js|jsx)$/,
                 exclude: /node_modules/,
                 use: {
